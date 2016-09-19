4b9f0a2b21a69d18adf2db6ed8d500e54c2bf33b
astroxslurg <kkrorkk@gmail.com>
2016-09-19 14:20:35 +0200

    annen version, fortsatt 5/6 korrekthet

--- oving3.py
+++ oving3.py
@@ -6,39 +6,31 @@ from itertools import repeat
 
 def merge(decks):
     deckLength = len(decks)
-    print(decks)
-    print (deckLength)
     deckIndexes = [0]*deckLength
-    print (deckIndexes)
     index = 1
     mergedDecks = []
-    #for i in range (deckLength) #vurder xrange
-    print('DECKS:')
-    for i in range(deckLength):
-        print (decks[i])
-    print('DECKS[i][k]')
-    for i in range(deckLength):
-        for k in range(len(decks[i])):
-            print(i, ': ', decks[i][k])
+    numberOfCards = 0
+    for k in decks:
+        numberOfCards += len(k)
 
     i = 0
-    while (i<deckLength):
+    while (index <= numberOfCards):
+        if (i == deckLength):
+            i = 0
+            index += 1
+            continue
+
         if (deckIndexes[i] >= len(decks[i])):
             i += 1
             continue
 
-        print('i:', end=' ')
-        print(i, end=' deckIndexes[i]: ')
-        print(deckIndexes[i], end=' deckNR: ')
-        print(decks[i][deckIndexes[i]][0])
-
         if (decks[i][deckIndexes[i]][0] == index):
             mergedDecks.append(decks[i][deckIndexes[i]][1])
-            print(decks[i][deckIndexes[i]][1])
             index += 1
             deckIndexes[i] += 1
             i = 0
             continue
+
         i += 1
 
     returnString = ''.join(mergedDecks)
