cb94e680fb8ef4ad708df7a8adb89e0558af7e81
astroxslurg <kkrorkk@gmail.com>
2016-10-04 19:27:24 +0200

    countingSort fully implemented

--- oving5.py
+++ oving5.py
@@ -9,51 +9,31 @@ from collections import defaultdict
 def alph(char):
     return (ord(char) - 97)
 
-def countingSort(arr):
+def countingSort(start, end, A, r):
     counter = [0] * 26
-    for a in arr:
-        counter[alph(a)] += 1
+    i = 0
+    for i in range(start, (end + 1)):
+        counter[alph(A[i][r])] += 1
     i = 0
     for i in range(26):
-        counter[i] = counter[i] + counter[i - 1]
+        counter[i] += counter[i - 1]
+
+    B = [0] * (end - start + 1)
+    i = 0
+    for i in range(end, (start - 1), -1):
+        print(A[i], 'r=', A[i][r], 'counter=', counter[alph(A[i][r])])
+        B[counter[alph(A[i][r])] - 1] = A[i]
+        counter[alph(A[i][r])] -= 1
 
     i = 0
-    [0] *
-    for in range(len(arr), 0, -1):
-
-
-    for a in range(27):
-        for c in range(counter[a]):
-            arr[i] = a
-            i += 1
-    return (arr,counter)
-
-def countingsort( aList, k ):
-    counter = [0] * ( k + 1 )
-    for i in aList:
-      counter[i] += 1
-
-    ndx = 0;
-    for i in range( len( counter ) ):
-      while 0 < counter[i]:
-        aList[ndx] = i
-        ndx += 1
-        counter[i] -= 1
-
-def insertionSort(A):
-  for i in range(1, len(A)):
-    tmp = A[i]
-    k = i
-    while k > 0 and tmp < A[k - 1]:
-        A[k] = A[k - 1]
-        k -= 1
-    A[k] = tmp
+    for i in range(end - start + 1):
+        A[i + start] = B[i]
 
 def stringSort(start, end, arr, radix):
 
 
 def flexradix(A, d):
-    stringSort(0, (len(A) - 1), A, 1)
+    stringSort(0, (len(A) - 1), A, 0)
 
     return A
 
