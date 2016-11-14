9d75b100534fe6cebeaee697ff1646de944bbfab
astroxslurg <kkrorkk@gmail.com>
2016-11-14 19:52:43 +0100

    working as intended

--- oving11.py
+++ oving11.py
@@ -1,22 +1,22 @@
 from sys import stdin
 
 
-# maxHeap is missing buildHeap, because it is not needed in this script
-class maxHeap:
+# MaxHeap is missing buildHeap, because it is not needed in this script
+class MaxHeap:
     def __init__(self):
         self.list = [0]
         self.size = 0
 
     def bubbleUp(self, index):
         while (index // 2) > 0:
-            if self.list[index] > self.list[index // 2]:
+            if self.list[index][0] > self.list[index // 2][0]:
                 tmp = self.list[index // 2]
                 self.list[index // 2] = self.list[index]
                 self.list[index] = tmp
             index = index // 2
 
-    def insert(self, item):
-        self.list.append(item)
+    def insert(self, prob, node):
+        self.list.append((prob, node))
         self.size = self.size + 1
         self.bubbleUp(self.size)
 
@@ -24,7 +24,7 @@ class maxHeap:
         if ((index * 2) + 1) > self.size:
             return index * 2
         else:
-            if self.list[index*2] > self.list[index*2+1]:
+            if self.list[index*2][0] > self.list[index*2+1][0]:
                 return index * 2
             else:
                 return index * 2 + 1
@@ -32,7 +32,7 @@ class maxHeap:
     def sinkDown(self, index):
         while (index * 2) <= self.size:
             mc = self.minChild(index)
-            if self.list[index] > self.list[mc]:
+            if self.list[index][0] < self.list[mc][0]:
                 tmp = self.list[index]
                 self.list[index] = self.list[mc]
                 self.list[mc] = tmp
@@ -42,34 +42,41 @@ class maxHeap:
         if self.size == 0:
             return -1
         retval = self.list[1]
+        print(retval)
         self.list[1] = self.list[self.size]
         self.size = self.size - 1
         self.list.pop()
         self.sinkDown(1)
         return retval
 
+    def list(self):
+        return self.list
+
 
 def best_path(nm, prob):
-    print('prob', prob)
-    q = deque([0])
+    print('prob:', prob)
+    h = MaxHeap()
+    h.insert(1, 0)
     totProb = [0]*len(prob)
     totProb[0] = prob[0]
     pi = [-1]*len(prob)
-    while(q):
-        node = q.pop()
+    p = len(pi) - 1
+    while(1 < len(h.list)):
+        node = h.pop()[1]
+        if node == p:
+            break
         for i, n in enumerate(nm[node]):
             if n:
                 tp = totProb[node]*prob[i]
                 print(node, '->', i, 'tp:', tp)
                 if (tp > totProb[i]):
                     totProb[i] = tp
-                    q.appendleft(i)
+                    h.insert(prob[i], i)
                     pi[i] = node
         print('totProb:', totProb)
         print('pi:', pi)
-        print('q:', q)
+        print('h:', h.list)
 
-    p = len(pi) - 1
     path = [str(p)]
     while True:
         p = pi[p]
