2f1112c42effb6cd161322026f0a628585c1b250
astroxslurg <kkrorkk@gmail.com>
2016-11-14 21:38:37 +0100

    all tests passed, but room for performance improvement

--- oving11.py
+++ oving11.py
@@ -53,7 +53,6 @@ class MaxHeap:
 
 
 def best_path(nm, prob):
-    print('prob:', prob)
     h = MaxHeap()
     h.insert(prob[0], 0)
     totProb = [0]*len(prob)
@@ -67,14 +66,10 @@ def best_path(nm, prob):
         for i, n in enumerate(nm[node]):
             if n:
                 tp = totProb[node]*prob[i]
-                print(node, '->', i, 'tp:', tp)
                 if (tp > totProb[i]):
                     totProb[i] = tp
-                    h.insert(prob[i], i)
+                    h.insert(tp, i)
                     pi[i] = node
-        print('totProb:', totProb)
-        print('pi:', pi)
-        print('h:', h.list)
 
     path = [str(p)]
     while True:
@@ -92,6 +87,9 @@ def best_path(nm, prob):
 def main():
     n = int(stdin.readline())
     probabilities = [float(x) for x in stdin.readline().split()]
+    neighbourLists = [[] for i in range(n)]
+    for line in stdin
+
     neighbour_matrix = []
     for line in stdin:
         neighbour_row = [0] * n
